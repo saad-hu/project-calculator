62982dc3c0917f2399ee6adc64a9ab69b097e71b
dodo <saad9191h@gmail.com>
2022-06-04 09:55:10 -0400

    modified calculate function to do all calculations in console. No safety features yet

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
+    if(b == 0) return null;
     return a/b;
 }
 
@@ -18,21 +19,53 @@ function divide(a,b) {
 function operate(a,b,operator) {
     if(operator == '+') return add(a,b);
     else if(operator == '-') return subtract(a,b);
-    else if(operator == '*') return multiply(a,b);
-    else if(operator == '/') return divide(a,b);
+    else if(operator == 'x') return multiply(a,b);
+    else if(operator == '÷') return divide(a,b);
     else return null;
 
 }
 
+let valuesArray = [];
 
 
-const keys = document.querySelectorAll('td');
+const keys = document.querySelectorAll('button.key');
 
 keys.forEach((key) => {
     key.addEventListener('click', () => {
-
-        console.log(key.textContent);
+        let userClick = key.textContent;
+        valuesArray.push(userClick);
+        console.log(userClick);
     })
-
 })
+
+let equalSign = document.querySelector('.equalSign');
+
+equalSign.addEventListener('click', () => {
+    calculate();
+})
+
+
+let operators = ['+', '-', 'x', '÷'];
+
+function calculate() {
+    let operator;
+
+    let operatorIndex = valuesArray.findIndex((value) => {
+        for(let i = 0; i < operators.length; i++) {
+
+            if(operators[i] == value) {
+                operator = value;
+                return true;
+            }
+            
+        }
+    });
+
+    let operand1 = valuesArray.slice(0,operatorIndex).join("");
+    let operand2 = valuesArray.slice(operatorIndex+1, valuesArray.length - 1).join("");
+
+    let result = operate(operand1, operand2, operator);
+    console.log(result);
+
+}
 
